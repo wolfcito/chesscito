ad6b94d9690e683ee8a6e4725f75f3affe3f4d90
Luis Fernando <guffenix@gmail.com>
2026-03-03 21:17:19 -0500

    fix(web): alias optional metamask modules for next

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@react-native-async-storage": false,
+      "@react-native-async-storage/async-storage": false,
+    }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
